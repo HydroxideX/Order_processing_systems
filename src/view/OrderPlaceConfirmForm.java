a51580044a1da9d4bab3dae62bb0449efd6ae8ab
HydroxideX <YahiaElsaadawy@gmail.com>
2021-06-09 07:20:01 +0200

    fixed GUI for place Order form

--- src/view/OrderPlaceConfirmForm.java
+++ src/view/OrderPlaceConfirmForm.java
@@ -31,7 +31,6 @@ public class OrderPlaceConfirmForm extends Application {
         vBox.getChildren().addAll(hBox,gridPane,hBox1);
         addUIControls(gridPane);
         Button place_order = componentsBuilder.buildButton(hBox1, "Place Order",40, 200, true);
-        Button confirm_order = componentsBuilder.buildButton(hBox1, "Confirm Order",  40, 200, true);
         Scene scene = new Scene(vBox, 800, 640);
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -47,12 +46,8 @@ public class OrderPlaceConfirmForm extends Application {
         TextField ISBN = componentsBuilder.addTextField(gridPane, 40, 2, 1);
         Label title_label = componentsBuilder.addLabel(gridPane,"Title", 3, 0);
         TextField title = componentsBuilder.addTextField(gridPane, 40, 3, 1);
-        Label username_label = componentsBuilder.addLabel(gridPane,"Username", 4, 0);
-        TextField username = componentsBuilder.addTextField(gridPane, 40, 4, 1);
-        Label copies_label = componentsBuilder.addLabel(gridPane,"Copies", 5, 0);
-        TextField copies = componentsBuilder.addTextField(gridPane, 40, 5, 1);
-        Label date_label = componentsBuilder.addLabel(gridPane,"Date", 6, 0);
-        TextField date = componentsBuilder.addTextField(gridPane, 40, 6, 1);
+        Label copies_label = componentsBuilder.addLabel(gridPane,"Copies", 4, 0);
+        TextField copies = componentsBuilder.addTextField(gridPane, 40, 4, 1);
     }
 
     public static void main(String[] args) {
